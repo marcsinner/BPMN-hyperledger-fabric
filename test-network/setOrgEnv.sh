c0c0c409968c62cb1281b8b7f5846b2fba7eb12b
Marc Sinner <guitarmax10@gmail.com>
2022-07-15 15:05:33 +0200

    modify cc script

--- test-network/setOrgEnv.sh
+++ test-network/setOrgEnv.sh
@@ -5,8 +5,8 @@
 
 
 
-# default to using Org1
-ORG=${1:-Org1}
+# default to using OrgClient
+ORG=${1:-OrgClient}
 
 # Exit on first error, print all commands.
 set -e
@@ -16,32 +16,32 @@ set -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 ORDERER_CA=${DIR}/test-network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
-PEER0_ORG1_CA=${DIR}/test-network/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
-PEER0_ORG2_CA=${DIR}/test-network/organizations/peerOrganizations/org2.example.com/tlsca/tlsca.org2.example.com-cert.pem
+PEER0_ORG1_CA=${DIR}/test-network/organizations/peerOrganizations/orgClient.example.com/tlsca/tlsca.orgClient.example.com-cert.pem
+PEER0_ORG2_CA=${DIR}/test-network/organizations/peerOrganizations/orgPlatform.example.com/tlsca/tlsca.orgPlatform.example.com-cert.pem
 PEER0_ORG3_CA=${DIR}/test-network/organizations/peerOrganizations/org3.example.com/tlsca/tlsca.org3.example.com-cert.pem
 
 
-if [[ ${ORG,,} == "org1" || ${ORG,,} == "digibank" ]]; then
+if [[ ${ORG,,} == "orgClient" || ${ORG,,} == "digibank" ]]; then
 
-   CORE_PEER_LOCALMSPID=Org1MSP
-   CORE_PEER_MSPCONFIGPATH=${DIR}/test-network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+   CORE_PEER_LOCALMSPID=OrgClientMSP
+   CORE_PEER_MSPCONFIGPATH=${DIR}/test-network/organizations/peerOrganizations/orgClient.example.com/users/Admin@orgClient.example.com/msp
    CORE_PEER_ADDRESS=localhost:7051
-   CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/test-network/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
+   CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/test-network/organizations/peerOrganizations/orgClient.example.com/tlsca/tlsca.orgClient.example.com-cert.pem
 
-elif [[ ${ORG,,} == "org2" || ${ORG,,} == "magnetocorp" ]]; then
+elif [[ ${ORG,,} == "orgPlatform" || ${ORG,,} == "magnetocorp" ]]; then
 
-   CORE_PEER_LOCALMSPID=Org2MSP
-   CORE_PEER_MSPCONFIGPATH=${DIR}/test-network/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+   CORE_PEER_LOCALMSPID=OrgPlatformMSP
+   CORE_PEER_MSPCONFIGPATH=${DIR}/test-network/organizations/peerOrganizations/orgPlatform.example.com/users/Admin@orgPlatform.example.com/msp
    CORE_PEER_ADDRESS=localhost:9051
-   CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/test-network/organizations/peerOrganizations/org2.example.com/tlsca/tlsca.org2.example.com-cert.pem
+   CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/test-network/organizations/peerOrganizations/orgPlatform.example.com/tlsca/tlsca.orgPlatform.example.com-cert.pem
 
 else
-   echo "Unknown \"$ORG\", please choose Org1/Digibank or Org2/Magnetocorp"
-   echo "For example to get the environment variables to set upa Org2 shell environment run:  ./setOrgEnv.sh Org2"
+   echo "Unknown \"$ORG\", please choose OrgClient/Digibank or OrgPlatform/Magnetocorp"
+   echo "For example to get the environment variables to set upa OrgPlatform shell environment run:  ./setOrgEnv.sh OrgPlatform"
    echo
    echo "This can be automated to set them as well with:"
    echo
-   echo 'export $(./setOrgEnv.sh Org2 | xargs)'
+   echo 'export $(./setOrgEnv.sh OrgPlatform | xargs)'
    exit 1
 fi
 
